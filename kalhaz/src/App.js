a617bc4bfab5cffb4b1d22af9d547ae077b4448a
merhawi <merhawi.temnewo88@gmail.com>
2019-03-12 10:58:41 -0500

    able to create header with react-mdl

--- kalhaz/src/App.js
+++ kalhaz/src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
-import HeaderCom from './components/HeaderCom';
+import { Layout, Header, Navigation, Drawer, Content } from 'react-mdl';
+
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <HeaderCom />
+      <div style={{ height: '300px', position: 'relative' }}>
+        <Layout fixedHeader>
+          <Header
+            title={
+              <span>
+                <span style={{ color: '#ddd' }}>Area / </span>
+                <strong>The Title</strong>
+              </span>
+            }
+          >
+            <Navigation>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+              <a href="#">Link</a>
+            </Navigation>
+          </Drawer>
+          <Content />
+        </Layout>
       </div>
     );
   }
